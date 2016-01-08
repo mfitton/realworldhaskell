18e5df03ac5433126173b008b3039e61cdd19015
Max Fitton <maxfitton@gmail.com>
2016-01-07 23:26:06 -0500

    did exercises 6 7 and 8 for Ch3. Hard but satisfying when they suddenly worked

--- ch3/endex1-10.hs
+++ ch3/endex1-10.hs
@@ -1,16 +1,33 @@
+import Data.List (sortBy)
+--exercises 1 and 2
 size :: [a] -> Int
 size (_:xs) = 1 + (size xs)
 size [] = 0
 
+--exercise 3
 mean :: [Int] -> Double
 mean xs = (fromIntegral (sum xs)) / (fromIntegral (length xs))
 
+--exercise 4
 makePalindrome :: [a] -> [a]
 makePalindrome xs = xs ++ (myreverse xs)
 makePalindrome [] = [] 
- 
-isPalindrome xs = myreverse xs == xs 
 
 myreverse :: [a] -> [a]
 myreverse (x:xs) = (myreverse xs) ++ [x]
 myreverse [] = []
+
+--exercise 5
+isPalindrome xs = myreverse xs == xs 
+
+--exercise 6
+sortListsByLength xs = (sortBy lengthcomp) xs
+                     where lengthcomp xs ys = (length xs) `compare` (length ys)
+
+--exercises 7 and 8
+intersperse :: a -> [[a]] -> [a]
+intersperse sep (x1 : x2 : xs)  = x1 ++ [sep] ++ intersperse sep (x2 : xs)
+intersperse sep [x1] = x1
+
+--exercise 9
+
